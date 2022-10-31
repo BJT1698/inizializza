9413baaf298ef0e6f32b7c2c01c413457e735c70
BJT1698 <32323748+BJT1698@users.noreply.github.com>
2022-10-31 15:44:11 +0100

    Update Inizializza.bat

--- Inizializza.bat
+++ Inizializza.bat
@@ -1,9 +1,38 @@
 @echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
 mkdir temp
 cd temp
 echo Inizializzazione in corso...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle', 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle')
-powershell -Command "Invoke-WebRequest https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+powershell -Command "Invoke-WebRequest https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 powershell Add-AppPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 cls
 echo 1) Si
@@ -49,7 +78,7 @@ IF ERRORLEVEL 1 GOTO Si
 
 
 :Si
-winget install -e --verbose-logs --id Adobe.AdobeAcrobatReaderDC
+winget install -e --verbose-logs --id Adobe.Acrobat.Reader.64-bit
 cls
 GOTO End
 :No
